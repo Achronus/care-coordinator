ee67faf5f6c03ab1e67578f6147d42e4e775ad7d
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-09 17:36:57 +0100

    Add `PatientForm` and `DynamicFormField` logic
    - Add template form logic and JSX to 'PatientForm.tsx'
    - Add packages for forms
    - Add form icons
    - Add form styles
    - Add 'shadcn/ui' components
    - Add 'FormFieldType' enum
    - Add 'DynamicFormField' JSX and 'RenderField' logic for 'PatientForm'

--- frontend/src/components/forms/PatientForm.tsx
+++ frontend/src/components/forms/PatientForm.tsx
@@ -1,5 +1,72 @@
+"use client";
+
+import DynamicFormField from "@/components/DynamicFormField";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { FormFieldType } from "@/types/enums";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
 const PatientForm = () => {
-  return <div>PatientForm</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Welcome Back!</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+
+        <DynamicFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <DynamicFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@youremail.com"
+          iconSrc="/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <DynamicFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(555) 123-4567"
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default PatientForm;

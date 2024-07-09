4115c9dc2640a16344833c0254814342e4861a5b
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-09 15:46:11 +0100

    Add `Homepage` template
    - Add 'typography' styles
    - Refactor 'layout' styles to separate file
    - Add 'Homepage' JSX
    - Add 'PatientForm' template
    - Update 'globals.css' with 'remove-scrollbar' and imports
    - Add 'logo' and 'onboarding' images

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,43 @@
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
+
 type Props = {};
 
 const Homepage = ({}: Props) => {
   return (
-    <main>
-      <section></section>
+    <main className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/logo.svg"
+            height={1000}
+            width={1000}
+            alt="Logo"
+            className="mb-12 h-10"
+          />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024 CareCoordinator
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/onboarding.png"
+        height={1000}
+        width={1000}
+        alt="Onboarding"
+        className="side-img max-w-[55%]"
+      />
     </main>
   );
 };

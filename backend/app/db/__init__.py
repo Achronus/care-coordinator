957951cf219e9740491ebd90a73a02437b92dd79
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-10 14:34:58 +0100

    Refactor `db` creation, add `CRUD` operations
    - Move 'config/database.py' -> 'db/__init__.py'
    - Add 'CRUD' class with methods
    - Move db init logic into function ('init_db')
    - Store db connections in model ('DBConnections')
    - Set collection CRUD models
    - Remove lifespan from 'main.py'

---
+++ backend/app/db/__init__.py
@@ -0,0 +1,62 @@
+from app.config.settings import settings
+
+from app.db.crud import CRUD
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+from appwrite.services.storage import Storage
+from appwrite.services.messaging import Messaging
+from appwrite.services.users import Users
+
+from pydantic import BaseModel
+
+
+class DBConnections(BaseModel):
+    """A model to access all database connections on server run."""
+
+    client: Client
+    db: Databases
+    storage: Storage
+    messaging: Messaging
+    users: Users
+
+
+def init_db() -> DBConnections:
+    """Initialise the database."""
+    client = Client()
+    client.set_endpoint("https://cloud.appwrite.io/v1")
+    client.set_project(settings.DB.PROJECT_ID)
+    client.set_key(settings.DB.API_KEY)
+
+    db = Databases(client)
+    storage = Storage(client)
+    messaging = Messaging(client)
+    users = Users(client)
+
+    return DBConnections(
+        client=client,
+        db=db,
+        storage=storage,
+        messaging=messaging,
+        users=users,
+    )
+
+
+connect = init_db()
+
+patient = CRUD(
+    db=connect.db,
+    db_id=settings.DB.ID,
+    collection_id=settings.DB.PATIENT_COLLECTION_ID,
+)
+
+doctor = CRUD(
+    db=connect.db,
+    db_id=settings.DB.ID,
+    collection_id=settings.DB.DOCTOR_COLLECTION_ID,
+)
+
+appointment = CRUD(
+    db=connect.db,
+    db_id=settings.DB.ID,
+    collection_id=settings.DB.APPOINTMENT_COLLECTION_ID,
+)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,8 @@
-from contextlib import asynccontextmanager
-
-from app.db import init_db
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    await init_db()
-    yield
-
-
-app = FastAPI(docs_url="/api/docs", redoc_url=None, lifespan=lifespan)
+app = FastAPI(docs_url="/api/docs", redoc_url=None)
 
 # app.include_router(root.router, prefix="/api")
 

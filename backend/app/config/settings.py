75132e863bbf10ad42b041c950f1f5fb5386e1d0
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-10 12:31:35 +0100

    Configure `appwrite` db to backend
    - Remove unused packages: 'beanie', 'aiohttp', 'requests'; add 'appwrite'
    - Refactor 'db/__init__.py' -> 'config/database.py' + add db config logic
    - Add 'DatabaseConfig' class to settings

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,17 +1,38 @@
 import os
 
 from app.utils.fileloader import FileLoader
+from pydantic import BaseModel
+
+
+class DatabaseConfig(BaseModel):
+    """A place to store the appwrite database settings."""
+
+    ID: str
+    PROJECT_ID: str
+    API_KEY: str
+    PATIENT_COLLECTION_ID: str
+    DOCTOR_COLLECTION_ID: str
+    APPOINTMENT_COLLECTION_ID: str
+    BUCKET_ID: str
 
 
 class Settings:
+    """A model for storing all config settings."""
+
     __fileloader = FileLoader()
 
     DIRPATHS = __fileloader.DIRPATHS
     FILEPATHS = __fileloader.FILEPATHS
 
-    DB_URL = os.getenv("DATABASE_URL")
-    DB_NAME = ""  # Update me!
-    DB_COLLECTION_NAME = ""  # Update me!
+    DB = DatabaseConfig(
+        ID=os.getenv("DB_ID"),
+        PROJECT_ID=os.getenv("APPWRITE_PROJECT_ID"),
+        API_KEY=os.getenv("APPWRITE_API_KEY"),
+        PATIENT_COLLECTION_ID=os.getenv("PATIENT_COLLECTION_ID"),
+        DOCTOR_COLLECTION_ID=os.getenv("DOCTOR_COLLECTION_ID"),
+        APPOINTMENT_COLLECTION_ID=os.getenv("APPOINTMENT_COLLECTION_ID"),
+        BUCKET_ID=os.getenv("BUCKET_ID"),
+    )
 
 
 settings = Settings()
